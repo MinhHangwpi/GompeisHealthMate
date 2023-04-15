87e5ba4065d1e5f0e7d7689dc3902b840272d11b
jpayvazian <jpayvazian@wpi.edu>
2023-04-15 13:18:26 -0400

    DV nutrient progress bars

--- app/src/main/java/com/example/cs528finalproject/utils/Constants.kt
+++ app/src/main/java/com/example/cs528finalproject/utils/Constants.kt
@@ -6,4 +6,17 @@ object Constants {
     const val EXERCISES: String = "exercises"
     const val MENUS: String = "menus"
     const val CHANNEL_ID: String = "your_id"
+
+    const val CALORIES: String = "calories"
+    const val CARBS: String = "carbs"
+    const val FAT: String = "fat"
+    const val FIBER: String = "fiber"
+    const val PROTEIN: String = "protein"
+    const val SUGAR: String = "sugar"
+    const val DV_CAL: Double = 2000.0
+    const val DV_CARBS: Double = 275.0
+    const val DV_FAT: Double = 78.0
+    const val DV_FIBER: Double = 28.0
+    const val DV_PROTEIN: Double = 50.0
+    const val DV_SUGAR: Double = 50.0
 }

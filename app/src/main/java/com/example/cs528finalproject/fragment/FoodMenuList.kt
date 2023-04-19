71d22c87e822900ffc8b2f286ad1890bb39242d0
MinhHang <mgradetsky@wpi.edu>
2023-04-19 02:01:13 -0400

    added a alertDiaglog when clicking on a Food Menu list

--- app/src/main/java/com/example/cs528finalproject/fragment/FoodMenuList.kt
+++ app/src/main/java/com/example/cs528finalproject/fragment/FoodMenuList.kt
@@ -1,6 +1,8 @@
 package com.example.cs528finalproject.fragment
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -16,11 +18,6 @@ import com.example.cs528finalproject.viewmodels.FoodLocationsViewModel
 import com.example.cs528finalproject.viewmodels.FoodMenusViewModel
 
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FoodMenuList.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FoodMenuList : Fragment() {
     private lateinit var binding: FragmentFoodMenuListBinding
 
@@ -54,6 +51,39 @@ class FoodMenuList : Fragment() {
         adapter = context?.let { FoodMenuListViewAdapter(foodMenu!!, it) }!!
         binding.foodMenuList.adapter = adapter
         binding.txtMenuListLocation.text = foodLocation?.name ?: "Menu"
+
+        binding.foodMenuList.setOnItemClickListener{ parent, view, position, id ->
+
+            Log.d("Food Menu List", "Menu item has been clicked: ${foodMenu?.get(position)}")
+
+            val builder = AlertDialog.Builder(context)
+            builder.setMessage("Do you want to consume this food?")
+                .setPositiveButton("Yes") { _, _ ->
+                    // Perform database update here
+                    updateMenuItem()
+                }
+                .setNegativeButton("No") { _, _ -> }
+                .show()
+        }
+
         return binding.root
+    }
+
+    private fun updateMenuItem() {
+
+        Log.d("MENU", "Pretending that menu update successful")
+//        // Perform database update here
+//        // For example, you can use Firebase Realtime Database to update the item
+//        val database = Firebase.database.reference
+//        val locationId = foodLocation?.id ?: ""
+//        val menuItemId = item.id ?: ""
+//        database.child("locations").child(locationId).child("menu").child(menuItemId)
+//            .setValue(item)
+//            .addOnSuccessListener {
+//                Toast.makeText(context, "Menu item updated", Toast.LENGTH_SHORT).show()
+//            }
+//            .addOnFailureListener {
+//                Toast.makeText(context, "Failed to update menu item", Toast.LENGTH_SHORT).show()
+//            }
     }
 }
